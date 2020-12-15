c9f4529e8c789125a39e0b957af8617c69a3538a
hosam <hosamelmalt@gmail.com>
2020-12-15 14:08:11 +0200

    Initial Commit

---
+++ Simple Music Player/Simple_Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Music_PlayerApp.swift
+//  Simple Music Player
+//
+//  Created by hosam on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
